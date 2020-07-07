081a3e67a09485a4fbbe83f8985c4aebe741a4e5
lisequesnel <quesnel.lise@gmail.com>
2020-07-07 17:56:47 +0200

    Add a different page for sup-students
    and rename students in sco-students

---
+++ orga/app/routes/authenticated/sup-students.js
@@ -0,0 +1,14 @@
+import { inject as service } from '@ember/service';
+import Route from '@ember/routing/route';
+
+export default class StudentsRoute extends Route {
+
+  @service currentUser;
+
+  beforeModel() {
+    super.beforeModel(...arguments);
+    if (!this.currentUser.isSUPManagingStudents) {
+      return this.replaceWith('application');
+    }
+  }
+}
