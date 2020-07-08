28c83f95fd78f320edd0c42c430931cddb3d3679
Guillaume Lagorce <guillaume@hey-gul.com>
2020-07-08 12:32:00 +0200

    WIP - Add event listener to handle auto reply

--- mon-pix/app/components/challenge-item-qroc.js
+++ mon-pix/app/components/challenge-item-qroc.js
@@ -6,12 +6,12 @@ import classic from 'ember-classic-decorator';
 class ChallengeItemQroc extends ChallengeItemGeneric {
 
   autoReplyAnswer = '';
+  postMessageHandler = null;
 
   _hasError() {
     return this._getAnswerValue().length < 1;
   }
 
-  // FIXME refactor that
   _getAnswerValue() {
     return this.showProposal ? (document.querySelector('[data-uid="qroc-proposal-uid"]')).value : this.autoReplyAnswer;
   }
@@ -28,6 +28,30 @@ class ChallengeItemQroc extends ChallengeItemGeneric {
   answerChanged() {
     this.set('errorMessage', null);
   }
+
+  didInsertElement() {
+    if (this.challenge.autoReply) {
+      this.addEventListener();
+    }
+  }
+
+  didDestroyElement() {
+    if (this.challenge.autoReply) {
+      window.removeEventListener('message', this.postMessageHandler);
+    }
+  }
+
+  addEventListener() {
+    this.postMessageHandler = this.receiveEmbedMessage.bind(this);
+    window.addEventListener('message', this.postMessageHandler);
+  }
+
+  receiveEmbedMessage(event) {
+    if (typeof event.data === 'string') {
+      this.autoReplyAnswer = event.data;
+    }
+  }
+
 }
 
 export default ChallengeItemQroc;
